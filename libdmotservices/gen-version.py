37733d39c9643e21528ba88e4f68c5a057baffa9
Denton <dentonescu@gmail.com>
2025-10-18 17:10:25 +0200

    comment correction

--- libdmotservices/gen-version.py
+++ libdmotservices/gen-version.py
@@ -4,7 +4,7 @@ root = pathlib.Path(__file__).resolve().cwd()
 ver = (root/"VERSION").read_text().strip()
 
 # C header
-for c_os in [ "linux" ]:    # C for now, but maybe I'll make a Windows C part in the future
+for c_os in [ "linux" ]:    # Linux for now, but maybe I'll make a Windows C part in the future
     cdir = root/"c"/c_os/"include"/"dmot"
     cdir.mkdir(parents=True, exist_ok=True)
     (root/"c"/c_os/"include"/"dmot"/"version.h").write_text(
